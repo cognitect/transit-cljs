2c3ada0225181a1511183d816b64fc1c0a012915
David Nolen <davidnolen@Davids-iMac.local>
2014-05-23 16:39:58 -0400

    include transit-cljs write time for seattle data

--- bench/transit/bench.cljs
+++ bench/transit/bench.cljs
@@ -7,6 +7,7 @@
 (def fs (js/require "fs"))
 (def json (. fs (readFileSync "../transit/seattle-data0.tjs" "utf8")))
 (def r (t/reader :json))
+(def w (t/writer :json))
 
 (println "100 iters, transit read seattle file")
 (println (. r (read json)))
@@ -19,8 +20,14 @@
   (dotimes [_ 100]
     (.parse js/JSON json)))
 
+(println "100 iters, transit write seattle file")
+(let [seattle (. r (read json))]
+  (time
+    (dotimes [_ 100] 
+      (.write w seattle))))
+
 (println "10 iters, cljs.reader/read-string seattle file")
-(def edn (pr-str (. r (read json))))
+(def edn (pr-str (.read r json)))
 (time
   (dotimes [_ 10]
     (r/read-string edn)))
