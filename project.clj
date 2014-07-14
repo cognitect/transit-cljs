7f367baf1b973111aa3a11c29841ac2352e980ef
David Nolen <dnolen@cognitect.com>
2014-07-14 14:36:38 -0400

    bump version, update tests

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2268"]
-                 [com.cognitect/transit-js "0.1.530"]]
+                 [com.cognitect/transit-js "0.1.531"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
             [codox "0.8.9"]]
