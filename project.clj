745719a1401a9a73551b20e3e966339dd827ef8e
David Nolen <dnolen@cognitect.com>
2014-08-08 19:10:56 -0400

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/cognitect/transit-cljs"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2277"]
-                 [com.cognitect/transit-js "0.8.665"]]
+                 [org.clojure/clojurescript "0.0-2307"]
+                 [com.cognitect/transit-js "0.8.702"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
             [codox "0.8.9"]]
