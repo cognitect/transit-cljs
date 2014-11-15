284dc46b2e5c637a7d1b2e8594f56ba0da137274
David Nolen <david.nolen@gmail.com>
2014-11-15 14:33:24 -0500

    bump transit-js dep

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
-                 [com.cognitect/transit-js "0.8.723"]]
+                 [com.cognitect/transit-js "0.8.746"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
             [codox "0.8.9"]]
