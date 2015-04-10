43b2d000dba881119e93a28a34a79685e23e2409
David Chelimsky <dchelimsky@gmail.com>
2015-04-10 12:32:19 -0500

    bump to transit-js-0.8.783 (fix json int boundaries)

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2665" :scope "provided"]
-                 [com.cognitect/transit-js "0.8.755"]]
+                 [com.cognitect/transit-js "0.8.783"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
             [codox "0.8.9"]]
